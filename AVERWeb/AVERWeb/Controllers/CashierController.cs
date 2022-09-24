746710ef1f8f9edba23f2b7558f3f6aa06510e51
MykolaKolbun <mykola.kolbun@gmail.com>
2022-09-25 00:41:57 +0300

    9

--- AVERWeb/AVERWeb/Controllers/CashierController.cs
+++ AVERWeb/AVERWeb/Controllers/CashierController.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Text.Json;
 using AVERWeb.Data;
 using AVERWeb.Models;
 using Microsoft.AspNetCore.Mvc;
